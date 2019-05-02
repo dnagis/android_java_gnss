4faf277c513bf9f8571d7126056728efcfc9ab19
dnagis <vincent.achard@gmail.com>
2019-05-02 16:21:38 +0200

    appel MainActivity depuis callback onLocationChanged, exple: update textview

--- src/com/example/android/hellogps/LocationVvnx.java
+++ src/com/example/android/hellogps/LocationVvnx.java
@@ -7,6 +7,8 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 
+
+
 import android.util.Log;
 
 public class LocationVvnx implements LocationListener {
@@ -14,40 +16,41 @@ public class LocationVvnx implements LocationListener {
 	private Context activityContext;
 	public LocationManager mLocationManager;
 	    
-	private static final int TEN_SECONDS = 10000;
-    private static final int TEN_METERS = 10;
+	private static final int MIN_TIME = 1000; //long: minimum time interval between location updates, in milliseconds
+    private static final int MIN_DIST = 1; //float: minimum distance between location updates, in meters
 	
+	//constructeur, récup context de l'activity qui seule peut appeler getSystemService
 	public LocationVvnx(Context mContext){
 		activityContext = mContext;
 		Log.d("vvnx", "constructeur classe location_vvnx");
 		setup();
-	 }
-	 
+	}	 
 	 
 	
     private void setup() {
-		Location gpsLocation = null;
-	
+		Location gpsLocation = null;	
 		mLocationManager = (LocationManager)activityContext.getSystemService(Context.LOCATION_SERVICE);
-		//if (mLocationManager == null) Log.d("vvnx", "lm est null dans setup");			
-	
+		//if (mLocationManager == null) Log.d("vvnx", "lm est null dans setup");		
 		requestUpdatesFromProvider();        
     }
     
+    
     public void requestUpdatesFromProvider() {
-        //Location location = null;
-        
-		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, TEN_SECONDS, TEN_METERS, this);
-		//location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        
-        //return location;
+		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DIST, this);
     }
     
     
-    //implements LocationListener     
+    public Location getLastLoc() {
+		 Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		 return location;
+	}
+    
+    
+    //implements LocationListener --> il faut les 4 méthodes     
     @Override	
     public void onLocationChanged(Location location) {
-        Log.d("vvnx", location.getLatitude() + ",  " + location.getLongitude());
+		HelloGPS.updateLocText(location);
+        Log.d("vvnx", location.getLatitude() + ",  " + location.getLongitude() + ",  " + 	location.getAccuracy() + ",  " + 	location.getAltitude() + ",  " + location.getTime());
     }
         
 	@Override
